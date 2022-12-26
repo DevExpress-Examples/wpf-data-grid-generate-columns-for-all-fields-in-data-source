9b1765bfa3af8848198a419441f7f4530b07cdea
AndreySlabov <andrey.slabov@devexpress.com>
2022-12-26 17:58:31 +0400

    Update Sample

--- CS/SimpleData.cs
+++ CS/SimpleData.cs
@@ -1,22 +1,27 @@
-﻿using System.Data;
+﻿using System.Collections.ObjectModel;
 
 namespace AutoPopulateColumns {
-    public class SimpleData {
-        static DataTable dt = new DataTable();
-        public static DataView Numbers {
-            get {
-                if(dt.Rows.Count == 0) CreateSimpleData();
-                return dt.DefaultView;
-            }
-        }
-        static void CreateSimpleData() {
-            dt.Columns.Add("col1");
-            dt.Columns.Add("col2");
-            dt.Rows.Add(new object[] { 1, "one" });
-            dt.Rows.Add(new object[] { 2, "two" });
-            dt.Rows.Add(new object[] { 3, "three" });
-            dt.Rows.Add(new object[] { 4, "four" });
-            dt.Rows.Add(new object[] { 5, "five" });
+    public class Customer {
+        public string Name { get; set; }
+        public string City { get; set; }
+    }
+    public class DataModel {
+        public static ObservableCollection<Customer> GetCustomers() {
+            ObservableCollection<Customer> people = new ObservableCollection<Customer>();
+            people.Add(new Customer() { Name = "Gregory S. Price", City = "Hong Kong" });
+            people.Add(new Customer() { Name = "Irma R. Marshall", City = "Madrid" });
+            people.Add(new Customer() { Name = "John C. Powell", City = "Los Angeles" });
+            people.Add(new Customer() { Name = "Christian P. Laclair", City = "London" });
+            people.Add(new Customer() { Name = "Karen J. Kelly", City = "Hong Kong" });
+            people.Add(new Customer() { Name = "Brian C. Cowling", City = "Los Angeles" });
+            people.Add(new Customer() { Name = "Thomas C. Dawson", City = "Madrid" });
+            people.Add(new Customer() { Name = "Angel M. Wilson", City = "Los Angeles" });
+            people.Add(new Customer() { Name = "Winston C. Smith", City = "London" });
+            people.Add(new Customer() { Name = "Harold S. Brandes", City = "Bangkok" });
+            people.Add(new Customer() { Name = "Michael S. Blevins", City = "Hong Kong" });
+            people.Add(new Customer() { Name = "Jan K. Sisk", City = "Bangkok" });
+            people.Add(new Customer() { Name = "Sidney L. Holder", City = "London" });
+            return people;
         }
     }
 }
---
+++ CS/ViewModel.cs
@@ -0,0 +1,11 @@
+﻿using DevExpress.Mvvm;
+using System.Collections.ObjectModel;
+
+namespace AutoPopulateColumns {
+    public class ViewModel : ViewModelBase {
+        public ViewModel() {
+            Source = DataModel.GetCustomers();
+        }
+        public ObservableCollection<Customer> Source { get; }
+    }
+}
