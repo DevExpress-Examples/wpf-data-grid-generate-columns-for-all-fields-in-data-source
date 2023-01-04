e7aef7a0311e07bf075f4a800affc16d36a476da
DevExpressExampleBot <fakeEmail>
2023-01-04 16:04:57 +0400

    Source auto update [skip ci]

--- VB/Window1.xaml.vb
+++ VB/Window1.xaml.vb
@@ -1,12 +1,12 @@
-﻿Imports Microsoft.VisualBasic
 Imports System.Windows
 
 Namespace AutoPopulateColumns
 
-	Partial Public Class Window1
-		Inherits Window
-		Public Sub New()
-			InitializeComponent()
-		End Sub
-	End Class
+    Public Partial Class Window1
+        Inherits Window
+
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
+    End Class
 End Namespace
